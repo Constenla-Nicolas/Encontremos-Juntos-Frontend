49b794f1f2e31205290bc907c1a9345762d1649a
Consti <constinic@gmail.com>
2023-06-16 12:34:28 -0300

    remaking map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss' ],
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    { title: 'Mostrar mapa', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Listar', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Desmarcar', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Contactar', url: '/folder/archived', icon: 'archive' },
+    // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
   
 
   ];
--- src/main.ts
+++ src/main.ts
@@ -15,35 +15,5 @@ const loader = new Loader({
   libraries: ["places"]
 });
 
-declare global{
- 
-  interface window{initMap:() => void;}
-  
-}
-
-function initMap(): void{
-  loader.importLibrary('maps').then(({Map})=> [new Map(document.getElementById("map") as HTMLElement,
-  {center : {lat:-34.607931,lng: -58.373219},
-    zoom: 8,
-})]);
-}
-
-// Loader l ;
-// let map: google.maps.Map;
-
-// function initMap(): void {
-//   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-// }
-
-// declare global {
-//   interface Window {
-//     initMap: () => void;
-//   }
-// }
-// window.initMap = initMap;
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
